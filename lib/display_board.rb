a2c07a48c33138fb3f3a36e8b9e73ec69b702448
Isai Bismark Cortez <isai0505@gmail.com>
2019-06-20 15:26:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 
 def display_board(board)
-  puts "#{board[0]} | #{board[1]} | #{board[2]}"
+  puts "#{board[0]}  | #{board[1]}  | #{board[2]}"
 
   #puts(rows[0])("   |   |   ")
   #puts(rows[1])("-----------")

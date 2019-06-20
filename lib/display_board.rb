9404af414492955e611a0085805ffeaf2354edae
Isai Bismark Cortez <isai0505@gmail.com>
2019-06-20 15:17:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
+  binding.pry
   #puts(rows[0])("   |   |   ")
   #puts(rows[1])("-----------")
   #puts(rows[2])("   |   |   ")

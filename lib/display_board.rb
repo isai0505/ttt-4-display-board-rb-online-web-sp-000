dbd1d167505d4cd87bf3deff7a5afd9383410619
Isai Bismark Cortez <isai0505@gmail.com>
2019-06-20 14:45:39 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board =
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  puts(rows[0]).to eq("   |   |   ")
+  puts(rows[1]).to eq("-----------")
+  puts(rows[2]).to eq("   |   |   ")
+  puts(rows[3]).to eq("-----------")
+  puts(rows[4]).to eq("   |   |   ")
 end
+
